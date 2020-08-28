c42fe6647cd1e1c362e57a822a5aabdb01f97b3b
Michael-Mbaya <gikundamike@gmail.com>
2020-08-29 01:00:33 +0300

    routes and routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page404Component } from './page404/page404.component';
+import { ReposComponent } from './repos/repos.component';
+import { UsersComponent } from './users/users.component';
+import { AboutComponent } from './about/about.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent},
+  { path: 'repos', component: ReposComponent},
+  {path: 'about', component: AboutComponent},
+  { path:'**', component:Page404Component},
+      //redirect
+  { path: '', redirectTo:"/about", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { ReposComponent } from './repos/repos.component';
+import { Page404Component } from './page404/page404.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    ReposComponent,
+    Page404Component,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
