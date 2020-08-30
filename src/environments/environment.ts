4bda53c1e9210a625405d20b4d854e250a77d8fe
Michael-Mbaya <gikundamike@gmail.com>
2020-08-31 01:45:44 +0300

    build error trial fix 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiKey : '485dc8f7643abb7ccb114e3711e319f68b562955',
   clientSecret:'d68ae6b91bb8d9b927a87ef084c5ba2468a9c2b9',
-  clientID:'25d69cfc147506112bc3'
+  clientID:'25d69cfc147506112bc3',
 };
 
 /*
