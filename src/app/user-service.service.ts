7222f17bb20199116e1ceadb2f5b3627e52fc0d7
Michael-Mbaya <gikundamike@gmail.com>
2020-08-30 16:15:53 +0300

    *ngIf tests on output

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import "rxjs";
 // import { Observable } from 'rxjs';
     //map is old version
 // import 'rxjs/add/operator/map';
